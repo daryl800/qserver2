6b168e3b3172d4fb97011e1dc5ad8f0b1b2fa071
daryl800 <darylng@Daryls-MacBook-Pro.local>
2021-05-11 22:58:17 +0200

    Updates

---
+++ precache-manifest.8eb030cc4b6916949f5f3950a2ea77af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da71c11a15120ebed9f9b7a773adfe1a",
+    "url": "/qserver2/index.html"
+  },
+  {
+    "revision": "f1f2d2cc260eec35b7ac",
+    "url": "/qserver2/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "972bce8914cfbb5a2b9b",
+    "url": "/qserver2/static/css/main.dcb96f3e.chunk.css"
+  },
+  {
+    "revision": "f1f2d2cc260eec35b7ac",
+    "url": "/qserver2/static/js/2.7ebe2716.chunk.js"
+  },
+  {
+    "revision": "0d68d4392bba0a65a7fa8c172929670f",
+    "url": "/qserver2/static/js/2.7ebe2716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972bce8914cfbb5a2b9b",
+    "url": "/qserver2/static/js/main.d7c08ac8.chunk.js"
+  },
+  {
+    "revision": "73ddc786b97faf21c8b8",
+    "url": "/qserver2/static/js/runtime-main.82d9f7ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/qserver2/static/media/logo.5d5d9eef.svg"
+  }
+]);
