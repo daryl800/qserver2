c95d9087bde1aff4ffc8492333c3498ca2b54172
daryl800 <darylng@Daryls-MacBook-Pro.local>
2021-05-16 13:44:55 +0200

    Updates

--- precache-manifest.8eb030cc4b6916949f5f3950a2ea77af.js
+++ precache-manifest.8eb030cc4b6916949f5f3950a2ea77af.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "764187d1761bc6e2ae41a301255c3ec9",
+    "revision": "da71c11a15120ebed9f9b7a773adfe1a",
     "url": "/qserver2/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/qserver2/static/css/2.a5f06b7f.chunk.css"
   },
   {
-    "revision": "3879f4c1b486950632eb",
-    "url": "/qserver2/static/css/main.7eff9057.chunk.css"
+    "revision": "972bce8914cfbb5a2b9b",
+    "url": "/qserver2/static/css/main.dcb96f3e.chunk.css"
   },
   {
     "revision": "f1f2d2cc260eec35b7ac",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/qserver2/static/js/2.7ebe2716.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3879f4c1b486950632eb",
-    "url": "/qserver2/static/js/main.bfc117c4.chunk.js"
+    "revision": "972bce8914cfbb5a2b9b",
+    "url": "/qserver2/static/js/main.d7c08ac8.chunk.js"
   },
   {
     "revision": "73ddc786b97faf21c8b8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/qserver2/precache-manifest.69eccca23a4082d1c24a6d376f6e9aa4.js"
+  "/qserver2/precache-manifest.8eb030cc4b6916949f5f3950a2ea77af.js"
 );
 
 self.addEventListener('message', (event) => {
